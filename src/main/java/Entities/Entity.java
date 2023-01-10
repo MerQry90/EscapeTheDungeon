47c5ac74041509e12ed6cce0ed6fe40115b27429
MitchLugli <lugli.michele.01@gmail.com>
2023-01-10 18:22:53 +0100

    Entity finito

--- src/main/java/Entities/Entity.java
+++ src/main/java/Entities/Entity.java
@@ -1,8 +1,64 @@
 package Entities;
 
+import javax.swing.*;
+import java.awt.*;
+
 public abstract class Entity {
 
-	private int x, y;
+	private int x;
+	private int y;
+	private int speed;
+	private int width;
+	private int height;
+	private Image sprite;
 	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
 	
+	public void moveUp(){
+		setY(getY() - getSpeed());
+	}
+	public void moveDown(){
+		setY(getY() + getSpeed());
+	}
+	public void moveLeft(){
+		setX(getX() - speed);
+	}
+	public void moveRight(){
+		setX(getX() + speed);
+	}
+	
+	public void setSprite(String path){
+		ImageIcon icon = new ImageIcon(path);
+		sprite = icon.getImage();
+	}
+	public Image getSprite(){
+		return sprite;
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(getSprite(), getX(), getY(), getWidth(), getHeight(), null);
+	}
 }
