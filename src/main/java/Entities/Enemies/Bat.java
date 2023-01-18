1d186746ad312b57294b47facb6c70113c7ebd5f
MerQry90 <mercu90@gmail.com>
2023-01-18 11:14:36 +0100

    Aggiunta dell'enemy "bat" (nome temporaneo)

---
+++ src/main/java/Entities/Enemies/Bat.java
@@ -0,0 +1,64 @@
+package Entities.Enemies;
+
+import static java.lang.Math.abs;
+
+public class Bat extends Enemy{
+
+	private int wait, countdown, movingCountdown;
+	int deltaX, deltaY;
+
+	public Bat(int x, int y){
+		super(x, y, 64, 64, 0.7, 0.9);
+
+		setMinimumSpeed(1); //l'estremo è escluso, velocità a cui viene sommata maximumSpeed
+		setMaximumSpeed(40); //verrà sommato a minimumSpeed
+		setSpeed(initializeRandomSpeed());
+		setMinimumHealth(2);
+		setMaximumHealth(1);
+		wait = 30;
+		countdown = 0;
+		movingCountdown = 20;
+		deltaX = 0;
+		deltaY = 0;
+
+		setHealth();
+		setSprite("src/resources/sprites/png/player_sample.png");
+	}
+
+	@Override
+	public void updateBehaviour(int playerX, int playerY) {
+		countdown++;
+
+		if(countdown <= wait){
+			deltaX = playerX - getX();
+			deltaY = playerY - getY();
+		}
+
+		double angle;
+		int traslX, traslY;
+		if(countdown >= wait) {
+			movingCountdown--;
+			if (abs(deltaX) >= abs(deltaY)) {
+				angle = Math.atan((double) abs(deltaY) / abs(deltaX));
+				traslX = (int) (getSpeed() * Math.cos(angle));
+				traslY = (int) (getSpeed() * Math.sin(angle));
+			} else {
+				angle = Math.atan((double) abs(deltaX) / abs(deltaY));
+				traslX = (int) (getSpeed() * Math.sin(angle));
+				traslY = (int) (getSpeed() * Math.cos(angle));
+			}
+			if (deltaX < 0) {
+				traslX = traslX * -1;
+			}
+			if (deltaY < 0) {
+				traslY = traslY * -1;
+			}
+			setX(keepXBoundaries(getX() + traslX));
+			setY(keepYBoundaries(getY() + traslY));
+			if(movingCountdown <= 0){
+				countdown = 0;
+				movingCountdown = 20;
+			}
+		}
+	}
+}
--- src/main/java/Stages/Stage.java
+++ src/main/java/Stages/Stage.java
@@ -1,5 +1,6 @@
 package Stages;
 
+import Entities.Enemies.Bat;
 import Entities.Enemies.Enemy;
 import Entities.Enemies.Zombie;
 
@@ -16,13 +17,12 @@ public class Stage {
 	}
 
 	public void loadRandomStage(List<Enemy> enemies){
-		stageNumber = random.nextInt(10);
+		stageNumber = random.nextInt(1);
 		switch(stageNumber){
 			case 0:
-				enemies.add(new Zombie(30, 30));
-				enemies.add(new Zombie(800, 30));
-				enemies.add(new Zombie(30, 400));
-				enemies.add(new Zombie(800, 400));
+				enemies.add(new Bat(64, 64));
+				//enemies.add(new Bat(800, 64));
+
 				break;
 			case 1:
 				enemies.add(new Zombie(30, 30));
